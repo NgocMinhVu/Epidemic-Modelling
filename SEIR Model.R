a628c2a8c90e6d5914a80397fbfb082bd33970e9
Minh Vu <ngocminhvu883@gmail.com>
2023-01-28 14:24:09 +0000

    ggplot2 wide format, long format, wes anderson

--- SEIR Model.R
+++ SEIR Model.R
@@ -24,10 +24,38 @@ model <- ode(initial_state, times, SEIR, params)
 
 summary(model)
 
-# Plot using matplot
+
+# matplot
 matplot(model, type='l', lty=1, main='SEIR model', xlab='Time')
 
 legend <- colnames(model)[2:6]
 legend('right', legend=legend, col=2:6, lty = 1)
 
 
+# Wide format
+require(ggplot2)
+theme_set(
+  theme_minimal() +
+    theme(legend.position='right')
+)
+
+model_df <- as.data.frame(model)
+
+ggplot(model_df, aes(x=time)) +
+  geom_line(aes(y=S, color='S')) +
+  geom_line(aes(y=E, color='E')) +
+  geom_line(aes(y=I, color='I')) +
+  geom_line(aes(y=R, color='R')) +
+  geom_line(aes(y=M, color='M')) + 
+  scale_color_manual(values = c( '#52854C', '#FFDB6D', '#D16103', '#4E84C4', '#293352'))
+
+
+# Long format
+require(tidyr)
+require(wesanderson)
+
+model_long <- model_df %>% gather(variable, value, -time)
+
+ggplot(model_long, aes(x=time, y=value, color=variable)) +
+  geom_line() +
+  scale_color_manual(values=wes_palette('Moonrise3', 5))
